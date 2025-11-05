78e3b62350fcae770df90b6741d13d05f9063713
son-daehyeon <sondaehyeon01@gmail.com>
2025-11-05 11:23:17 +0900

    feat(club): export club entity

---
+++ src/entities/club/index.ts
@@ -0,0 +1,2 @@
+export type { Club, ClubCategory, ClubType, ClubStatus, ClubRanking, CompetitionRate, FilterState } from "./model/types"
+export { mockClubs, getClubById, calculateCompetitionRate, getViewsRankings, getLikesRankings } from "./model/mock-data"
